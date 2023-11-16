bb3d1a147637ab919cba019507ed9d3f14a2bff1
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-16 23:05:24 +0900

    Configファイル編集 & アプリケーション変数の設定
    下記を実行してみよ
    `$ rails runner "p LearnRails::Application.config.developper"`

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module LearnRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.developper = "Nyantama"
   end
 end
