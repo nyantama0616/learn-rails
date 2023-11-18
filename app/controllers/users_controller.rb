3946ac1b46188453a8c06554d4ce2a5d9b24b2d3
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-18 16:08:02 +0900

    フィクスチャ使ってみた
    テストデータを投入できる
    `$ rails db:fixtures:load`

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+    def index
+        users = User.all
+        result = users.as_json(only: [:id, :user_id, :password])
+        render json: {users: result}
+    end
+
     def show
         user = User.find(params[:id])
         render json: {user: user}
